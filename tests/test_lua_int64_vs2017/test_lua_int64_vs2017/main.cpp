22be7d6fda19f7c40b55cc4954d77571937515a2
denghe <denghe@hotmail.com>
2017-11-29 01:36:38 +0800

    remove mempool, BBuffer add  lua 5.1 with tolua int64 uint64 support, simple test ok

--- tests/test_lua_int64_vs2017/test_lua_int64_vs2017/main.cpp
+++ tests/test_lua_int64_vs2017/test_lua_int64_vs2017/main.cpp
@@ -1,37 +1,27 @@
-﻿#include <lua.hpp>
-#ifdef _WIN64
-#pragma comment(lib, "..\\lualib\\x64\\lua51.lib")
-#else
-#pragma comment(lib, "..\\lualib\\x86\\lua51.lib")
-#endif
-#include <stdint.h>
+﻿#include <stdint.h>
 #include <assert.h>
 #include <iostream>
-
-extern "C"
-{
-	void tolua_openint64(lua_State* L);
-	int  tolua_newint64(lua_State* L);
-	void tolua_pushint64(lua_State* L, int64_t n);
-
-	void tolua_openuint64(lua_State* L);
-	int  tolua_newuint64(lua_State* L);
-	//void tolua_pushuint64(lua_State* L, uint64_t n);	// tolua.h 中没这句
-}
+#include "lua_xx.h"
 
 int TestInt64(lua_State *L)
 {
 	luaL_openlibs(L);
 	tolua_openint64(L);
+	tolua_openuint64(L);
+	xxlua_openxx(L);
 	if (luaL_dostring(L, R"--(
 
-local a = int64.new()
-print( a )
-a = 10
-a = a * 2
+local a = uint64.new()
+a = 1
 local b = a
-b = b + 1
+a = 2
 print( a, b )
+
+local bb = BBuffer.Create()
+bb:WriteUInt64( a, b )
+print( bb )
+
+print( bb:ReadUInt64( 2 ) )
 
 )--")) return lua_error(L);
 	return 0;
@@ -45,7 +35,9 @@ int main()
 	auto rtv = lua_pcall(L, 0, 0, 0);
 	if (rtv != LUA_OK)
 	{
-		std::cout << "pcall rtv = " << rtv << ", errmsg = " << lua_tostring(L, -1) << std::endl;
+		std::cout << "pcall rtv = " << rtv
+			<< ", errmsg = " << lua_tostring(L, -1) 
+			<< std::endl;
 		lua_pop(L, 1);
 	}
 	lua_close(L);
